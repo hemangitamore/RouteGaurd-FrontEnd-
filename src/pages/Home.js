f71df3c7f760a639754106919c0f06427e9661e4
Hemangi <hemangitamore7@gmail.com>
2024-08-08 00:42:25 +0530

    Saved

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
     return (
         <>
             <Hero />
-            <AdminLogin/>
+         
             <Services />
             <Portfolio />
             <Clients />
